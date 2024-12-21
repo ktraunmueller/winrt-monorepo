d160a2e410560c9be6914be268797c80d95ec35c
Karl Traunmüller <karl@microlarge.com>
2024-12-21 15:40:18 +0100

    project now builds successfully

--- Package.swift
+++ Package.swift
@@ -6,16 +6,20 @@ import PackageDescription
 let package = Package(
     name: "WinAppSDK",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "WinAppSDK",
-            type: .dynamic,
-            targets: ["WinAppSDK"]),
+            name: "WinAppSDK", type: .dynamic, targets: ["WinAppSDK"]),
+    ],
+    dependencies: [
+        .package(path: "../WindowsFoundation"),
+        .package(path: "../UWP")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WinAppSDK")
-    ]
+            name: "WinAppSDK",
+            dependencies: [
+                .product(name: "WindowsFoundation", package: "WindowsFoundation"),
+                .product(name: "UWP", package: "UWP")
+            ])
+    ],
+    swiftLanguageModes: [.v5]
 )
