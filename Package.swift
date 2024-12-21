e9ad3fd54e6ce2f9a39f5b688cae8e3bf5aeb013
Karl Traunmüller <karl@microlarge.com>
2024-12-21 14:21:41 +0100

    fixed incorrect project name in projections.json

--- Package.swift
+++ Package.swift
@@ -8,9 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CWinRT",
-            type: .dynamic,
-            targets: ["CWinRT"]),
+            name: "CWinRT", type: .dynamic, targets: ["CWinRT"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
--- Sources/WindowsFoundation/Generated/Support/WINRTDELEGATE.swift
+++
@@ -1,4 +0,0 @@
-// WinRTDelegateBridge specifies the contract for bridging between WinRT and Swift for event handlers and d
-public protocol WinRTDelegateBridge<Handler>: AbiInterfaceBridge where SwiftProjection == Handler {
-    associatedtype Handler
-}
