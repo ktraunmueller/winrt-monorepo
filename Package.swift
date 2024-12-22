6d63c0ee8ec7d6b8c05f530ef6a8ad8371e023bc
Karl Traunmüller <karl@microlarge.com>
2024-12-22 15:47:14 +0100

    added missing files

--- Package.swift
+++ Package.swift
@@ -3,23 +3,41 @@
 
 import PackageDescription
 
+let linkerSettings: [LinkerSetting] = [
+/* Figure out magic incantation so we can delay load these dlls
+    .unsafeFlags(["-L\(currentDirectory)/Sources/CWinAppSDK/nuget/lib"]),
+    .unsafeFlags(["-Xlinker" , "/DELAYLOAD:Microsoft.WindowsAppRuntime.Bootstrap.dll"]),
+*/
+]
+
 let package = Package(
     name: "WinAppSDK",
     products: [
-        .library(
-            name: "WinAppSDK", type: .dynamic, targets: ["WinAppSDK"]),
+        .library(name: "WinAppSDK", type: .dynamic, targets: ["WinAppSDK"]),
+        .library(name: "CWinAppSDK", targets: ["CWinAppSDK"]),
     ],
     dependencies: [
+        .package(path: "../CWinRT"),
+        .package(path: "../UWP"),
         .package(path: "../WindowsFoundation"),
-        .package(path: "../UWP")
     ],
     targets: [
         .target(
             name: "WinAppSDK",
             dependencies: [
+                .product(name: "CWinRT", package: "CWinRT"),
+                .product(name: "UWP", package: "UWP"),
                 .product(name: "WindowsFoundation", package: "WindowsFoundation"),
-                .product(name: "UWP", package: "UWP")
-            ])
+                "CWinAppSDK"
+            ]
+        ),
+        .target(
+            name: "CWinAppSDK",
+            resources: [
+                .copy("nuget/bin/Microsoft.WindowsAppRuntime.Bootstrap.dll"),
+            ],
+            linkerSettings: linkerSettings
+        )
     ],
     swiftLanguageModes: [.v5]
 )
