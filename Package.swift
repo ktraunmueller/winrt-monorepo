1a6a9901d894a5cd250af057d8c37e9e4ec83c11
Karl Traunmüller <karl@microlarge.com>
2024-12-24 15:29:12 +0100

    updated Windows App SDK

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CWinRT",
+    name: "swift-cwinrt",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
