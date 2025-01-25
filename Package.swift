a4a5224faf08cec87c8fac31736ac57b2ad10f14
Karl Traunmüller <karl@microlarge.com>
2025-01-25 11:15:45 +0100

    temporarily moving to local package paths

--- Package.swift
+++ Package.swift
@@ -10,10 +10,14 @@ let package = Package(
             name: "WinUI", type: .dynamic, targets: ["WinUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ktraunmueller/swift-cwinrt", branch: "main"),
-        .package(url: "https://github.com/ktraunmueller/swift-uwp", branch: "main"),
-        .package(url: "https://github.com/ktraunmueller/swift-windowsappsdk", branch: "main"),
-        .package(url: "https://github.com/ktraunmueller/swift-windowsfoundation", branch: "main")
+        // .package(url: "https://github.com/ktraunmueller/swift-cwinrt", branch: "main"),
+        // .package(url: "https://github.com/ktraunmueller/swift-uwp", branch: "main"),
+        // .package(url: "https://github.com/ktraunmueller/swift-windowsappsdk", branch: "main"),
+        // .package(url: "https://github.com/ktraunmueller/swift-windowsfoundation", branch: "main")
+        .package(path: "../swift-cwinrt"),
+        .package(path: "../swift-uwp"),
+        .package(path: "../swift-windowsappsdk"),
+        .package(path: "../swift-windowsfoundation")
     ],
     targets: [
         .target(
