e9501d792867633b7606ca6c4992b29e1fd4e626
KARLS-SURFACE\karl <karl@microlarge.com>
2025-03-30 12:11:21 +0200

    minor clean up

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-cwinrt",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CWinRT", type: .dynamic, targets: ["CWinRT"]),
+            name: "CWinRT", 
+            type: .dynamic, 
+            targets: ["CWinRT"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
