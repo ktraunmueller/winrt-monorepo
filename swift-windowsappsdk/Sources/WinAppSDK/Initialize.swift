1b31f33b4187c59bcd4124fbb918fa9d3738b45b
Karl Traunmüller <karl@microlarge.com>
2025-10-18 10:39:42 +0200

    replaced MainRunLoopTickler with new Application with proper RunLoop integration (copied from TBC swift-winui repo)

--- swift-windowsappsdk/Sources/WinAppSDK/Initialize.swift
+++ swift-windowsappsdk/Sources/WinAppSDK/Initialize.swift
@@ -1,5 +1,6 @@
 import CWinRT
 import CWinAppSDK
+import Foundation
 import WindowsFoundation
 import WinSDK
 
@@ -27,7 +28,19 @@ public class WindowsAppRuntimeInitializer {
     // TODO: Figure out how to properly link against delayimp.lib so that we can delay load the bootstrap dll.
     private typealias pfnMddBootstrapInitialize2 = @convention(c) (UInt32, PCWSTR?, PACKAGE_VERSION, MddBootstrapInitializeOptions) -> HRESULT
     private typealias pfnMddBootstrapShutdown = @convention(c) () -> Void
-    private let bootstrapperDll = LoadLibraryA("swift-windowsappsdk_CWinAppSDK.resources\\Microsoft.WindowsAppRuntime.Bootstrap.dll")
+    private lazy var bootstrapperDll: HMODULE = {
+        guard let url = Bundle.main.url(
+            forResource: "Microsoft.WindowsAppRuntime.Bootstrap",
+            withExtension: ".dll",
+            subdirectory: "swift-windowsappsdk_CWinAppSDK.resources") else {    
+            preconditionFailure("Microsoft.WindowsAppRuntime.Bootstrap.dll not found!")
+        }
+        let path = url.withUnsafeFileSystemRepresentation { String(cString: $0!) }
+        guard let module = path.withCString(encodedAs: UTF16.self, LoadLibraryW) else {
+            preconditionFailure("Failed to load Microsoft.WindowsAppRuntime.Bootstrap.dll")
+        }
+        return module
+    }()
 
     private lazy var Initialize: pfnMddBootstrapInitialize2 = {
         let pfn = GetProcAddress(bootstrapperDll, "MddBootstrapInitialize2")
@@ -44,7 +57,14 @@ public class WindowsAppRuntimeInitializer {
         return GetCurrentPackageFullName(&length, nil) != APPMODEL_ERROR_NO_PACKAGE
     }
 
-    public init(threadingModel: ThreadingModel = .single) throws  {
+    private let selfContained: Bool
+
+    private lazy var initWinAppSDK: Bool = {
+        !processHasIdentity() && !selfContained
+    }()
+
+    public init(threadingModel: ThreadingModel = .single, selfContained: Bool = false) throws  {
+        self.selfContained = selfContained
         let roInitParam = switch threadingModel {
             case .single: RO_INIT_SINGLETHREADED
             case .multi: RO_INIT_MULTITHREADED
@@ -54,10 +74,13 @@ public class WindowsAppRuntimeInitializer {
 
         try CHECKED(SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE))
 
-        guard !processHasIdentity() else {
+        guard initWinAppSDK else {
             return
         }
 
+        let ver = String(decoding: UnsafeBufferPointer(start: WINDOWSAPPSDK_RELEASE_VERSION_TAG_SWIFT, count: wcslen(WINDOWSAPPSDK_RELEASE_VERSION_TAG_SWIFT)), as: UTF16.self)
+
+        print("initialize: \(ver)")
         try CHECKED(Initialize(
             UInt32(WINDOWSAPPSDK_RELEASE_MAJORMINOR),
             WINDOWSAPPSDK_RELEASE_VERSION_TAG_SWIFT,
@@ -69,8 +92,8 @@ public class WindowsAppRuntimeInitializer {
     }
 
     deinit {
-        RoUninitialize()
-        if !processHasIdentity() {
+        RoUninitialize()    
+        if initWinAppSDK {
             Shutdown()
         }
         FreeLibrary(bootstrapperDll)
--- swift-winui/Sources/WinUI/Application/SwiftApplication.swift
+++ swift-winui/Sources/WinUI/Application/SwiftApplication.swift
@@ -1,7 +1,7 @@
-import WinAppSDK
-@_spi(WinRTImplements) import WindowsFoundation
-
 import Foundation
+import WinAppSDK
+import WinSDK
+@_spi(WinRTImplements) import WindowsFoundation
 
 /// You should derive from SwiftApplication and mark this class as your @main entry point. This class
 /// will ensure that the Windows Runtime is properly initialized and that your WinUI Application
@@ -23,11 +23,8 @@ import Foundation
 ///    window.activate()
 ///   }
 /// ```
-open class SwiftApplication: /*Microsoft.UI.Xaml.*/Application, IXamlMetadataProvider {
-
-    public var applicationLogger: ((_ msg: String) -> Void)?
-
-    public private(set) var mainDispatcherQueue: DispatcherQueue?
+open class SwiftApplication: Application, IXamlMetadataProvider {
+    public typealias RunLoop = (DispatcherQueue) throws -> Int32
 
     public required override init() {
         super.init()
@@ -39,30 +36,83 @@ open class SwiftApplication: /*Microsoft.UI.Xaml.*/Application, IXamlMetadataPro
         onLaunched(args!)
     }
 
+    /// If you've setup your project to be self-contained WinAppSDK
+    open class var windowsAppSdkSelfContained: Bool { false }
+
+    /// If you own your own run loop and don't want to use the WinUI one, override it here
+    open class var runLoop: RunLoop { Self.defaultRunLoop }
+
     /// Override this method to provide your application's main entry point.
     /// The first window for your application should be created and activated here.
     open func onLaunched(_ args: LaunchActivatedEventArgs) {
     }
 
-    /// Override this method to provide any necessary shutdown code.
-    open func onShutdown() { }
+    private typealias pfnContentPreTranslateMessage = @convention(c) (UnsafePointer<MSG>?) -> Bool
+    private static let contentPreTranslateMessage: pfnContentPreTranslateMessage = {
+        let windowingDLL = LoadLibraryA("Microsoft.UI.Windowing.Core.dll")
+        let contentPreTranslateMessage = GetProcAddress(windowingDLL, "ContentPreTranslateMessage")
+        return unsafeBitCast(contentPreTranslateMessage!, to: pfnContentPreTranslateMessage.self)
+    }()
 
+    private static func defaultRunLoop(_ queue: DispatcherQueue) -> Int32 {
+        // The below run loop is taken mostly from https://github.com/compnerd/swift-win32/blob/d34ff1b8b3f15cfdf2cb71109a3c313001122a54/Sources/SwiftWin32/App%20and%20Environment/ApplicationMain.swift#L183
+        // with some tweaks for WinUI
+        var msg: MSG = MSG()
+        while true {
+
+            // Process all messages in thread's message queue; for GUI applications UI
+            // events must have high priority.
+            while PeekMessageW(&msg, nil, 0, 0, UINT(PM_REMOVE)) {
+                if msg.message == UINT(WM_QUIT) {
+                    return Int32(msg.wParam)
+                }
+
+                if (!contentPreTranslateMessage(&msg)) {
+                    TranslateMessage(&msg)
+                    DispatchMessageW(&msg)
+                }
+            }
+
+            var time: Date? = nil
+            repeat {
+            // Execute Foundation.RunLoop once and determine the next time the timer
+            // fires.  At this point handle all Foundation.RunLoop timers, sources and
+            // Dispatch.DispatchQueue.main tasks
+            time = Foundation.RunLoop.main.limitDate(forMode: .default)
+
+            // If Foundation.RunLoop doesn't contain any timers or the timers should
+            // not be running right now, we interrupt the current loop or otherwise
+            // continue to the next iteration.
+            } while (time?.timeIntervalSinceNow ?? -1) <= 0
+
+            // Yield control to the system until the earlier of a requisite timer
+            // expiration or a message is posted to the runloop.
+            _ = MsgWaitForMultipleObjects(0, nil, false,
+                                        DWORD(exactly: time?.timeIntervalSinceNow ?? -1)
+                                            ?? 0,
+                                        QS_ALLINPUT)
+        }
+        return 0
+    }
+
+    /// Override this method to provide any necessary shutdown code.
+    open func onShutdown(exitCode: Int32) { }
+    
     public static func main() {
         do {
-            try withExtendedLifetime(WindowsAppRuntimeInitializer()) {
-                let appClass = String(describing: String(reflecting: Self.self))
-                guard let instance = NSClassFromString(appClass) else {
-                    fatalError("unable to find application class \(appClass)")
-                }
+            let appClass = String(describing: String(reflecting: Self.self))
+            guard let instance = NSClassFromString(appClass) as? SwiftApplication.Type else {
+                fatalError("unable to find application class \(appClass)")
+            }
+
+            try withExtendedLifetime(WindowsAppRuntimeInitializer(selfContained: instance.windowsAppSdkSelfContained)) {
                 var application: SwiftApplication!
-                try Application.start { _ in
-                    MainRunLoopTickler.setup()
-                    application = (instance as! SwiftApplication.Type).init()
-                    MainRunLoopTickler.instance.logger = application.applicationLogger 
-                    application.mainDispatcherQueue = try? DispatcherQueue.getForCurrentThread()
+                let createAppCallback: (ApplicationInitializationCallbackParams?) -> Void = { _ in
+                    application = instance.init()
                 }
-                application.onShutdown()
-                MainRunLoopTickler.shutdown()
+                let exitCode = try Application.startWithCustomRunLoop(instance.runLoop, createAppCallback)
+              
+                application.onShutdown(exitCode: exitCode)
             }
         }
         catch {
@@ -81,7 +131,6 @@ open class SwiftApplication: /*Microsoft.UI.Xaml.*/Application, IXamlMetadataPro
     }
 
     private lazy var metadataProvider: XamlControlsXamlMetaDataProvider = .init()
-
     public func getXamlType(_ type: TypeName) throws -> IXamlType! {
         try metadataProvider.getXamlType(type)
     }
@@ -91,6 +140,6 @@ open class SwiftApplication: /*Microsoft.UI.Xaml.*/Application, IXamlMetadataPro
     }
 
     public func getXmlnsDefinitions() throws -> [XmlnsDefinition] {
-        return try metadataProvider.getXmlnsDefinitions()
+        try metadataProvider.getXmlnsDefinitions()
     }
 }
