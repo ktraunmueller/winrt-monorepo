e16d5db7d8a6c254d8d150bf668c97eb79225851
Karl Traunmüller <karl@microlarge.com>
2025-11-14 12:32:28 +0100

    modified scripts

---
+++ configure-release.bat
@@ -0,0 +1,5 @@
+:: Note: run in a developer command prompt
+cmake -DCMAKE_BUILD_TYPE=Release ^
+    -S . -B build -G "Ninja" ^
+    -DSWIFT_TOOLCHAIN_PATH="E:/Swift/Toolchains/0.0.0+Asserts/usr" ^
+    -DCMAKE_Swift_COMPILER="E:/Swift/Toolchains/0.0.0+Asserts/usr/bin/swiftc.exe"
--- configure.bat
+++
@@ -1,2 +0,0 @@
-:: Note: run in a developer command prompt
-cmake -S . -B build -G "Ninja" -DCMAKE_Swift_COMPILER="E:\Swift\Toolchains\0.0.0+Asserts\usr\bin\swiftc.exe"
